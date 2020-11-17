58bd29c108258ea7c91ecd72b04a65755a1c222d
Cyrus Chan <cyruschan1212@gmail.com>
2020-11-17 11:13:35 +0800

    Finish setup file

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 import setuptools
 
+with open("README.md", "r") as readme_file:
+    long_desc = readme_file
+
 setuptools.setup(
     name="unicode-string-contain-check",
     version="1.0.0",
     author="rk0cc",
     author_email="cyruschan1212@gmail.com",
+    description="String contain validation for non-Latin language system",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rk0cc/unicode-string-contain-check",
+    package=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing",
+    ],
+    python_requires=">=3.8",
 )
